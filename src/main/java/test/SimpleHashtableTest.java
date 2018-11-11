15372732fa567ff71479ba89c5575193a031fecc
sskender <sskender@users.noreply.github.com>
2018-11-12 00:21:48 +0100

    Testing

---
+++ src/main/java/test/SimpleHashtableTest.java
@@ -0,0 +1,106 @@
+package test;
+
+import hashtable.SimpleHashtable;
+import org.junit.Test;
+
+public class SimpleHashtableTest {
+
+    SimpleHashtable<String, Integer> examMarks = new SimpleHashtable<>(2);
+
+    @Test
+    public void testEmpty() {
+        assert examMarks.isEmpty();
+    }
+
+    @Test
+    public void testEmptySize() {
+        assert examMarks.size() == 0;
+    }
+
+    @Test
+    public void testSizeAfterPut() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 2);
+        examMarks.put("Jasna", 2);
+        examMarks.put("Kristina", 5);
+        assert examMarks.size() == 4;
+    }
+
+    @Test
+    public void testValueUpdate() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 2);
+        examMarks.put("Jasna", 2);
+        examMarks.put("Kristina", 5);
+        examMarks.put("Ivana", 5);
+        Integer ivanaGrade = examMarks.get("Ivana");
+        assert ivanaGrade == 5;
+    }
+
+    @Test
+    public void testValueUpdateSize() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 2);
+        examMarks.put("Jasna", 2);
+        examMarks.put("Kristina", 5);
+        examMarks.put("Ivana", 5);
+        assert examMarks.size() == 4;
+    }
+
+    @Test
+    public void testContainsKey() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 3);
+        assert examMarks.containsKey("Ivana");
+        assert examMarks.containsKey("Ante");
+        assert !examMarks.containsKey("Jasna");
+    }
+
+    @Test
+    public void testContainsValue() {
+        examMarks.put("Ivana", 3);
+        assert examMarks.containsValue(3);
+        assert !examMarks.containsValue(2);
+    }
+
+    @Test
+    public void testRemoveNull() {
+        examMarks.remove("Jasna");
+        assert !examMarks.containsKey("Jasna");
+    }
+
+    @Test
+    public void testRemoveFirst() {
+        examMarks.put("Jasna", 5);
+        examMarks.remove("Jasna");
+        assert examMarks.size() == 0;
+        assert examMarks.get("Jasna") == null;
+    }
+
+    @Test
+    public void testRemoveWhenOverflow1() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 2);
+        examMarks.put("Jasna", 2);
+        examMarks.put("Kristina", 5);
+        examMarks.put("Ivana", 5);
+        examMarks.remove("Kristina");
+        assert !examMarks.containsKey("Kristina");
+        assert examMarks.containsKey("Ivana");
+        assert examMarks.size() == 3;
+    }
+
+    @Test
+    public void testRemoveWhenOverflow2() {
+        examMarks.put("Ivana", 2);
+        examMarks.put("Ante", 2);
+        examMarks.put("Jasna", 2);
+        examMarks.put("Kristina", 5);
+        examMarks.put("Ivana", 5);
+        examMarks.remove("Ivana");
+        assert !examMarks.containsKey("Ivana");
+        assert examMarks.containsKey("Kristina");
+        assert examMarks.size() == 3;
+    }
+
+}
